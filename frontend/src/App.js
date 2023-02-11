e7c6037509f6a0a78ad651b44c38de337e65006a
Prem Sagar <prem_saagar@ymail.com>
2023-02-11 11:22:40 +0500

    added more views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Button } from 'react-bootstrap';
 import Header from './Header';
 import { render } from "react-dom";
 import {BrowserRouter,Routes, Route } from "react-router-dom";
+import Home from './Home';
 import Login from './Login';
 import Register from './Register';
 import AddProduct from './AddProduct';
@@ -12,15 +13,17 @@ import UpdateProduct from './UpdateProduct';
 function App() {
   return (
     <div className="App">
-    
+      
       <BrowserRouter>
       <Header />
+      
       <Routes>
       
-
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/add-product" element={<AddProduct />} />
+      <Route path="/update-product" element={<UpdateProduct />} />
 
       </Routes>
       </BrowserRouter>
---
+++ frontend/src/Home.js
@@ -0,0 +1,11 @@
+function Home()
+{
+	return (
+		<div>
+			<h1>Welcome</h1>
+      <h2>to simple React Laravel Application</h2>
+		</div>
+	)
+}
+
+export default Home

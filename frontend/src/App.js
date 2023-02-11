529463d698ad53e024637dd99e317e9c9134d384
Prem Sagar <prem_saagar@ymail.com>
2023-02-11 10:06:01 +0500

    main component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
+import Header from './Header';
+import { render } from "react-dom";
+import {BrowserRouter,Routes, Route } from "react-router-dom";
+import Login from './Login';
+import Register from './Register';
+import AddProduct from './AddProduct';
+import UpdateProduct from './UpdateProduct';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <button className="btn btn-primary">Button</button>
-      <Button>Bootstrap </Button>
+    
+      <BrowserRouter>
+      <Header />
+      <Routes>
+      
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
